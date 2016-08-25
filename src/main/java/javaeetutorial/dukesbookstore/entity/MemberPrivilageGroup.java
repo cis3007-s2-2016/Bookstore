e475b7bc806f61f633683307c66a99fc7c37a721
Kyle M Lewer <kylelt@users.noreply.github.com>
2016-08-25 22:33:33 +1000

    Add two more entities Member privilage group and Privilage group

---
+++ src/main/java/javaeetutorial/dukesbookstore/entity/MemberPrivilageGroup.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaeetutorial.dukesbookstore.entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Kyle.Lewer
+ */
+@Entity
+@Table(name="member_privalage_groups")
+public class MemberPrivilageGroup implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @ManyToOne
+    private PrivilageGroup group;
+    
+    @ManyToOne
+    private Member groupMember;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public PrivilageGroup getGroup() {
+        return group;
+    }
+
+    public void setGroup(PrivilageGroup group) {
+        this.group = group;
+    }
+
+    public Member getGroupMember() {
+        return groupMember;
+    }
+
+    public void setGroupMember(Member groupMember) {
+        this.groupMember = groupMember;
+    }
+    
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof MemberPrivilageGroup)) {
+            return false;
+        }
+        MemberPrivilageGroup other = (MemberPrivilageGroup) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "javaeetutorial.dukesbookstore.entity.MemberPrivilageGroup[ id=" + id + " ]";
+    }
+    
+}
