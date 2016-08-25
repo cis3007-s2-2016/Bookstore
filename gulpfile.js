3e70f5ca790db3e958adead9ef4869647eb89a7f
Matt Gowland <u1038652@umail.usq.edu.au>
2016-08-25 18:57:58 +1000

    Add gulp support, modify sass files to suit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('sass', function() {
+    return gulp.src('src/main/webapp/resources/sass/main.scss')
+    	.pipe(sourcemaps.init())
+        .pipe(sass({includePaths:['node_modules/bootstrap-sass/assets/stylesheets/']}))
+        .pipe(rename('styles.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('src/main/webapp/resources/stylesheets/'));
+});
