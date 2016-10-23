d669f759986341798844052d37585f3a4e402078
Kyle M Lewer <kylelt@users.noreply.github.com>
2016-10-23 22:38:40 +1000

    the error was a stray annotation applying a size constraint to a boolean EE DID NOT LIKE

--- src/main/java/javaeetutorial/dukesbookstore/entity/SaleUsed.java
+++ src/main/java/javaeetutorial/dukesbookstore/entity/SaleUsed.java
@@ -29,6 +29,7 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.faces.convert.Converter;
+import javax.validation.constraints.NotNull;
 
 /**
  *
@@ -104,7 +105,6 @@ public class SaleUsed implements Serializable, Converter {
 	
 	@Column(name = "COMMISSION", nullable = false, precision = 7, scale = 2)    // Creates the database field with this size.
 	private BigDecimal commission;
-	@Size(max = 255)
 	
 	@Column(name = "PAID")
 	private boolean paid;
