95461a58eff5443b21055e9304052993c1fe4a2c
Matt Gowland <u1038652@umail.usq.edu.au>
2016-10-24 00:08:16 +1000

    Start auction listing page

---
+++ src/main/java/javaeetutorial/dukesbookstore/web/managedbeans/AuctionListingsBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaeetutorial.dukesbookstore.web.managedbeans;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author matt
+ */
+@Named(value = "auctionListingsBean")
+@Dependent
+public class AuctionListingsBean {
+
+	/**
+	 * Creates a new instance of AuctionListingsBean
+	 */
+	public AuctionListingsBean() {
+	}
+	
+}
